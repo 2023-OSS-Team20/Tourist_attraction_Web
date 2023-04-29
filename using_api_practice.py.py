aa72c95177b75b4ad06e21bafc1563475005d8a4
cmc4647 <127467135+cmc4647@users.noreply.github.com>
2023-04-29 21:57:36 +0900

    api 사용법 연습

---
+++ using_api_practice.py.py
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+  <response>
+    <header>
+      <resultCode>0000</resultCode>
+      <resultMsg>OK</resultMsg>
+    </header>
+    <body>
+      <items>
+        <item>
+          <galContentId>2859292</galContentId>
+          <galContentTypeId>17</galContentTypeId>
+          <galTitle>1100고지습지</galTitle>
+          <galWebImageUrl>http://tong.visitkorea.or.kr/cms2/website/92/2859292.jpg</galWebImageUrl>
+          <galCreatedtime>20220926105242</galCreatedtime>
+          <galModifiedtime>20220926105253</galModifiedtime>
+          <galPhotographyMonth>202207</galPhotographyMonth>
+          <galPhotographyLocation>제주특별자치도 서귀포시</galPhotographyLocation>
+          <galPhotographer>한국관광공사 이범수</galPhotographer>
+          <galSearchKeyword>1100고지습지, 제주특별자치도 서귀포시, 제주도 오름, 제주오름, 1100고지 탐방로</galSearchKeyword>
+        </item>
+      </items>
+      <numOfRows>1</numOfRows>
+      <pageNo>1</pageNo>
+      <totalCount>5251</totalCount>
+    </body>
+  </response>
