60e929e89aad03ce19e7d48850112c432bd2f076
HYUHO LEE <leehh41563636@gmail.com>
2023-04-30 14:46:36 +0900

    api 주소 수정

--- lee_API.py
+++ lee_API.py
@@ -1,2 +1,3 @@
 import json
 import requests
+url = "http://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey=71wIrgVtoA73X2mMZ5VzxKcPT7cUGUdUle%2Be0w%2BwmCc5AO%2F6Oion4qdMPIKWLRlfJFzymt8YCoDf%2FAtpMoSMwA%3D%3D&numOfRows=17&pageNo=1&MobileOS=ETC&MobileApp=TestApp&_type=json"
