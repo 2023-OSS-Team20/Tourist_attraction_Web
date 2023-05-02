b35bb4cce3a5eed6818e9a2682538d8fb9412c5a
HYUHO LEE <leehh41563636@gmail.com>
2023-05-02 22:26:34 +0900

    URL 수정 dict 변환

--- lee_API.py
+++ lee_API.py
@@ -3,14 +3,26 @@ import requests
 from urllib.parse import urlencode, quote_plus
 from urllib.request import urlopen
 
-serviceKey = "71wIrgVtoA73X2mMZ5VzxKcPT7cUGUdUle%2Be0w%2BwmCc5AO%2F6Oion4qdMPIKWLRlfJFzymt8YCoDf%2FAtpMoSMwA%3D%3D"
-url = "http://apis.data.go.kr/B551011/KorService1/areaCode1"
-queryParms = '?' + urlencode({quote_plus('areaCode') : None,
-                              quote_plus('servicekey') : serviceKey,
+Key = "71wIrgVtoA73X2mMZ5VzxKcPT7cUGUdUle%2Be0w%2BwmCc5AO%2F6Oion4qdMPIKWLRlfJFzymt8YCoDf%2FAtpMoSMwA%3D%3D"
+
+#URL 생성
+url = "https://apis.data.go.kr/B551011/KorService1/areaCode1"
+queryParms = '?' + urlencode({
+                              quote_plus('areaCode') : None,
+                              quote_plus('serviceKey') : Key,
                               quote_plus('numOfRows') : '17',
                               quote_plus('pageNo') : '1',
-                              quote_plus('MobileOs') : 'ETC',
+                              quote_plus('MobileOS') : 'ETC',
                               quote_plus('MobileApp') : 'TestApp',
-                              quote_plus('type') : 'json'})
+                              quote_plus('type') : 'json'
+                            })
+#최종 URL
+Qurl = url + queryParms
 
-response = urlopen(url + queryParms)
+
+reqData = requests.get(Qurl)
+print(reqData.text)
+
+r_dict=json.loads(reqData.text)
+
+#rDict = json.load(Qurl)
