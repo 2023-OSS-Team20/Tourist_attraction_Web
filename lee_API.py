5281008c807ac7b2bdac002d28fcba3839ef8472
HYUHO LEE <leehh41563636@gmail.com>
2023-05-04 15:42:39 +0900

    url 검색방식으로 수정, 변수 이름 변경확인

--- lee_API.py
+++ lee_API.py
@@ -1,28 +1,41 @@
 import json
 import requests
-from urllib.parse import urlencode, quote_plus
+from urllib.parse import urlencode, quote_plus, unquote
 from urllib.request import urlopen
 
 Key = "71wIrgVtoA73X2mMZ5VzxKcPT7cUGUdUle%2Be0w%2BwmCc5AO%2F6Oion4qdMPIKWLRlfJFzymt8YCoDf%2FAtpMoSMwA%3D%3D"
 
+
+
 #URL 생성
-url = "https://apis.data.go.kr/B551011/KorService1/areaCode1"
-queryParms = '?' + urlencode({
-                              quote_plus('areaCode') : None,
-                              quote_plus('serviceKey') : Key,
-                              quote_plus('numOfRows') : '17',
-                              quote_plus('pageNo') : '1',
-                              quote_plus('MobileOS') : 'ETC',
-                              quote_plus('MobileApp') : 'TestApp',
-                              quote_plus('type') : 'json'
-                            })
-#최종 URL
-Qurl = url + queryParms
+URL = "http://apis.data.go.kr/B551011/KorService1/searchKeyword1"
+#지역 코드변수
+Area = "서울"
+Town = "강남구"
+#검색키워드
+keyWord = "카페"
 
 
-reqData = requests.get(Qurl)
+queryParams = '?' + urlencode({
+                              quote_plus('MobileOS'): 'WIN',
+                              quote_plus('numOfRows') : '10',
+                              quote_plus('pageNo') : '1',                               
+                              quote_plus('MobileApp'): 'AppTest',
+                              quote_plus('serviceKey'): Key,
+                              quote_plus('_type') : 'json',
+                              quote_plus('arrange') : 'A' ,   #제목 순 정렬
+                              quote_plus('areaCode') : Area,
+                              quote_plus('sigunguCode') : Town,
+                              quote_plus('keyword' ): keyWord
+                              })
+
+search_url = URL + queryParams
+
+
+
+reqData = requests.get(search_url)
 print(reqData.text)
 
-r_dict=json.loads(reqData.text)
+#r_dict=json.loads(reqData.text)
 
-#rDict = json.load(Qurl)
+#rDict = json.load(Qurl)s
