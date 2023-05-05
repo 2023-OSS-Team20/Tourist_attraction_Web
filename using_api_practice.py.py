846359fc847c7ebc9a312efcba1b87ecdd6ab284
cmc4647 <127467135+cmc4647@users.noreply.github.com>
2023-05-05 22:20:28 +0900

    Update using_api_practice.py.py

--- using_api_practice.py.py
+++ using_api_practice.py.py
@@ -4,3 +4,11 @@ import urllib.request
 serviceKey  = '3Nb0/4jy/aClfnlxLxtzqVHBrvqyF6AJ6DUemOzRK+XG2zjBWs35+Y2p290TbSABSuFU43hTShYnqHMtPakdGA=='
 
 URL = 'http://apis.data.go.kr/B551011
+
+-------
+import urllib.request
+key = "Nb0%2F4jy%2FaClfnlxLxtzqVHBrvqyF6AJ6DUemOzRK%2BXG2zjBWs35%2BY2p290TbSABSuFU43hTShYnqHMtPakdGA%3D%3D"
+url = "http://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey=key&numOfRows=10&pageNo=1&MobileOS=AND&MobileApp=appName"
+
+content = urllib.request.request.get(url).content
+print(content)
