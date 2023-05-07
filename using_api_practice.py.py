22ea13491412358213032fc99c5dd2a52a0884dc
cmc4647 <127467135+cmc4647@users.noreply.github.com>
2023-05-07 21:21:48 +0900

    json 데이터 가공 연습

--- using_api_practice.py.py
+++ using_api_practice.py.py
@@ -1,20 +1,3 @@
-import json
-import urllib.request
-
-serviceKey  = '3Nb0/4jy/aClfnlxLxtzqVHBrvqyF6AJ6DUemOzRK+XG2zjBWs35+Y2p290TbSABSuFU43hTShYnqHMtPakdGA=='
-
-URL = 'http://apis.data.go.kr/B551011
-
--------
-import urllib.request
-key = "Nb0%2F4jy%2FaClfnlxLxtzqVHBrvqyF6AJ6DUemOzRK%2BXG2zjBWs35%2BY2p290TbSABSuFU43hTShYnqHMtPakdGA%3D%3D"
-url = "http://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey=key&numOfRows=10&pageNo=1&MobileOS=AND&MobileApp=appName"
-
-content = urllib.request.request.get(url).content
-print(content)
-
-
--------
 import requests
 import pprint
 import json
@@ -39,3 +22,34 @@ import pandas as pd
 dataframe = pd.json_normalize(body)
 
 print(dataframe)
+
+------
+# json 데이터 가공 연습 
+import json
+
+# json 파일 읽어오기
+with open('./test.json', 'r', encoding='UTF8') as json_file:
+    sample_json = json.load(json_file)
+    print(sample_json)
+
+# 출력: {'이름': '홍길동', '나이': 25, '특기': ['농구', '도술'], '가족관계': {'아버지': '홍판서', '어머니': '춘섬'}, '결혼 여부': True}
+
+# json 문자열 읽어오기
+json_str = '''
+{
+    "이름": "홍길동", 
+    "나이": 25, 
+    "특기": ["농구", "도술"], 
+    "가족관계": {
+        "아버지": "홍판서", 
+        "어머니": "춘섬"
+        }, 
+    "결혼 여부": true
+}
+'''
+sample_json2 = json.loads(json_str)
+print(sample_json2)
+# 출력: {'이름': '홍길동', '나이': 25, '특기': ['농구', '도술'], '가족관계': {'아버지': '홍판서', '어머니': '춘섬'}, '결혼 여부': True}
+
+
+
