7eb83a9c9b66062f0a9d9c91adbd76d499355eac
HYUHO LEE <leehh41563636@gmail.com>
2023-05-10 15:26:57 +0900

    serviecekry, keyword 입력등 해결

--- lee_API.py
+++ lee_API.py
@@ -5,8 +5,10 @@ from urllib.request import urlopen
 
 Key = "nzhfKajoHJmAX1gKiu5WyxWx2fbAfCxBWwOThdrb323YQrUlrW%2B1CmlnI3zWzZvdWwSHpP6665%2F8JdWBK1Pe4g%3D%3D"
 
+
+'''
 #URL 생성
-URL = "http://apis.data.go.kr/B551011/KorService1/searchKeyword1"
+URL = "http://apis.data.go.kr/B551011/KorService1/"
 #지역 코드변수
 Area = "서울"
 Town = "강남구"
@@ -19,18 +21,18 @@ queryParams = '?' + urlencode({
                               quote_plus('numOfRows') : '10',
                               quote_plus('pageNo') : '1',                               
                               quote_plus('MobileApp'): 'AppTest',
-                              quote_plus('serviceKey'): Key,
+                              quote_plus('serviceKey'): {Key},
                               quote_plus('_type') : 'json',
                               quote_plus('arrange') : 'A' ,   #제목 순 정렬
-                              quote_plus('areaCode') : Area,
-                              quote_plus('sigunguCode') : Town,
-                              quote_plus('keyword' ): keyWord
+                              quote_plus('areaCode') : {Area},
+                              quote_plus('sigunguCode') : {Town},
+                              quote_plus('keyword' ): {keyWord}
                               })
 
 
 
 search_url = URL + queryParams
-
+'''
 
 #struct keyword
 
@@ -42,9 +44,13 @@ class Linkmaker:
     pass
   
 #location linkmaker
+
 #keyword linkmaker
 
-
+class KeywordLink(Linkmaker):
+  def __init__(self):
+    self.url = "http://apis.data.go.kr/B551011/KorService1/"
+  
 
 
 
