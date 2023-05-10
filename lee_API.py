b8c54ada5e32ba55d62cf76b0fa6ed671402bac1
HYUHO LEE <leehh41563636@gmail.com>
2023-05-10 16:39:30 +0900

    keyword link class화, keyword encode 문제

--- lee_API.py
+++ lee_API.py
@@ -5,7 +5,6 @@ from urllib.request import urlopen
 
 Key = "nzhfKajoHJmAX1gKiu5WyxWx2fbAfCxBWwOThdrb323YQrUlrW%2B1CmlnI3zWzZvdWwSHpP6665%2F8JdWBK1Pe4g%3D%3D"
 
-
 '''
 #URL 생성
 URL = "http://apis.data.go.kr/B551011/KorService1/"
@@ -40,23 +39,45 @@ search_url = URL + queryParams
 class Linkmaker:
   def __init__(self):
     pass
-  def make(self, keyword):
+  def search(self, keyword):
     pass
+   
   
 #location linkmaker
 
 #keyword linkmaker
-
 class KeywordLink(Linkmaker):
   def __init__(self):
-    self.url = "http://apis.data.go.kr/B551011/KorService1/"
+    self.url = "http://apis.data.go.kr/B551011/KorService1/searchKeyword1?"
+    self.queryParams = "serviceKey=" + Key + '&' + urlencode({
+      quote_plus('MobileApp'): 'AppTest',
+      quote_plus('MobileOS'): 'ETC',
+      quote_plus('pageNo') : '1',
+      quote_plus('numOfRows') : '10',                               
+      quote_plus('_type') : 'json',
+      quote_plus('listYN') : 'Y', 
+      quote_plus('arrange') : 'A' ,   #제목 순 정렬
+    })
+    self.keyword = None
+    self.searchURL = None
+    
+    
+    
+  def search(self,keyword):
+    self.word = urlencode({quote_plus('keyword') : {keyword}})
+    print(self.word)
+    self.searchURL = self.url + self.queryParams + '&' + self.word
   
+link = KeywordLink()
+link.search("강릉")
+ 
+search_url = link.searchURL 
 
-
+print(search_url)
 
 reqData = requests.get(search_url)
 print(reqData.text)
 
 #r_dict=json.loads(reqData.text)
 
-rDict = json.load(search_url)
+#rDict = json.load(search_url)
