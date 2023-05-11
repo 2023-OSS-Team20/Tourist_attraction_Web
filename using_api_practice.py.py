36df16c594999d69eff38a7e131e3663355cf567
cmc4647 <127467135+cmc4647@users.noreply.github.com>
2023-05-11 14:26:19 +0900

    dummy

--- using_api_practice.py.py
+++ using_api_practice.py.py
@@ -23,34 +23,6 @@ dataframe = pd.json_normalize(body)
 
 print(dataframe)
 
-------
-# json 데이터 가공 연습 
-import json
-
-# json 파일 읽어오기
-with open('./test.json', 'r', encoding='UTF8') as json_file:
-    sample_json = json.load(json_file)
-    print(sample_json)
-
-# 출력: {'이름': '홍길동', '나이': 25, '특기': ['농구', '도술'], '가족관계': {'아버지': '홍판서', '어머니': '춘섬'}, '결혼 여부': True}
-
-# json 문자열 읽어오기
-json_str = '''
-{
-    "이름": "홍길동", 
-    "나이": 25, 
-    "특기": ["농구", "도술"], 
-    "가족관계": {
-        "아버지": "홍판서", 
-        "어머니": "춘섬"
-        }, 
-    "결혼 여부": true
-}
-'''
-sample_json2 = json.loads(json_str)
-print(sample_json2)
-# 출력: {'이름': '홍길동', '나이': 25, '특기': ['농구', '도술'], '가족관계': {'아버지': '홍판서', '어머니': '춘섬'}, '결혼 여부': True}
-
 -------
 import json
 
@@ -68,5 +40,4 @@ city = data['city']
 print(name)
 print(age)
 print(city)
-
 
