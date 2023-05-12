20284065d45d6f019d7a127d4487080acf63c179
LEE HYUNHO <127092247+MillionKiwi@users.noreply.github.com>
2023-05-12 13:13:27 +0900

    각주 제거 및 서치 수정

--- lee_API.py
+++ lee_API.py
@@ -5,34 +5,6 @@ from urllib.request import urlopen
 
 Key = "nzhfKajoHJmAX1gKiu5WyxWx2fbAfCxBWwOThdrb323YQrUlrW%2B1CmlnI3zWzZvdWwSHpP6665%2F8JdWBK1Pe4g%3D%3D"
 
-'''
-#URL 생성
-URL = "http://apis.data.go.kr/B551011/KorService1/"
-#지역 코드변수
-Area = "서울"
-Town = "강남구"
-#검색키워드
-keyWord = "카페"
-
-
-queryParams = '?' + urlencode({
-                              quote_plus('MobileOS'): 'WIN',
-                              quote_plus('numOfRows') : '10',
-                              quote_plus('pageNo') : '1',                               
-                              quote_plus('MobileApp'): 'AppTest',
-                              quote_plus('serviceKey'): {Key},
-                              quote_plus('_type') : 'json',
-                              quote_plus('arrange') : 'A' ,   #제목 순 정렬
-                              quote_plus('areaCode') : {Area},
-                              quote_plus('sigunguCode') : {Town},
-                              quote_plus('keyword' ): {keyWord}
-                              })
-
-
-
-search_url = URL + queryParams
-'''
-
 #struct keyword
 
 #abstract link class
@@ -58,7 +30,7 @@ class KeywordLink(Linkmaker):
       quote_plus('listYN') : 'Y', 
       quote_plus('arrange') : 'A' ,   #제목 순 정렬
     })
-    self.keyword = None
+    self.word = None
     self.searchURL = None
     
     
