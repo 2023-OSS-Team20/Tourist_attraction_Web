2ab9f06763a342177e83e5cad0bd7ff6a2d78756
csj0606 <csjoon0606@gmail.com>
2023-05-14 22:22:46 +0900

    5.14

---
+++ tourweb/tourapp/static/js/script.js
@@ -0,0 +1,31 @@
+document.getElementById("span1").addEventListener("mouseover", function() {
+      document.getElementById("bcat1").classList.add("show1");
+  });
+  
+  document.getElementById("span1").addEventListener("mouseout", function() {
+    document.getElementById("bcat1").classList.remove("show1");
+  });
+
+document.getElementById("span2").addEventListener("mouseover", function() {
+    document.getElementById("bcat2").classList.add("show1");
+});
+
+document.getElementById("span2").addEventListener("mouseout", function() {
+  document.getElementById("bcat2").classList.remove("show1");
+});
+
+document.getElementById("span3").addEventListener("mouseover", function() {
+  document.getElementById("bcat3").classList.add("show1");
+});
+
+document.getElementById("span3").addEventListener("mouseout", function() {
+document.getElementById("bcat3").classList.remove("show1");
+});
+
+document.getElementById("span4").addEventListener("mouseover", function() {
+  document.getElementById("bcat4").classList.add("show1");
+});
+
+document.getElementById("span4").addEventListener("mouseout", function() {
+document.getElementById("bcat4").classList.remove("show1");
+});
