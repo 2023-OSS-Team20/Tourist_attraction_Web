214c52ddca5113b6c1843c9e47c1ed960632aed9
csj0606 <csjoon0606@gmail.com>
2023-05-15 23:10:54 +0900

    5.15

--- tourweb/tourapp/static/js/script.js
+++ tourweb/tourapp/static/js/script.js
@@ -1,31 +1,16 @@
-document.getElementById("span1").addEventListener("mouseover", function() {
-      document.getElementById("bcat1").classList.add("show1");
+// 모든 부모 div 요소를 선택합니다.
+const parentDivs = document.querySelectorAll(".span1");
+
+parentDivs.forEach(parentDiv => {
+  const hiddenDiv = parentDiv.querySelector(".bcat1");
+
+  parentDiv.addEventListener("mouseover", function() {
+    hiddenDiv.style.display = "block";
+    parantDiv.style.opacity = "0,5";
   });
-  
-  document.getElementById("span1").addEventListener("mouseout", function() {
-    document.getElementById("bcat1").classList.remove("show1");
+
+  parentDiv.addEventListener("mouseout", function() {
+    hiddenDiv.style.display = "none";
+    parantDiv.style.opacity = "1"; 
   });
-
-document.getElementById("span2").addEventListener("mouseover", function() {
-    document.getElementById("bcat2").classList.add("show1");
-});
-
-document.getElementById("span2").addEventListener("mouseout", function() {
-  document.getElementById("bcat2").classList.remove("show1");
-});
-
-document.getElementById("span3").addEventListener("mouseover", function() {
-  document.getElementById("bcat3").classList.add("show1");
-});
-
-document.getElementById("span3").addEventListener("mouseout", function() {
-document.getElementById("bcat3").classList.remove("show1");
-});
-
-document.getElementById("span4").addEventListener("mouseover", function() {
-  document.getElementById("bcat4").classList.add("show1");
-});
-
-document.getElementById("span4").addEventListener("mouseout", function() {
-document.getElementById("bcat4").classList.remove("show1");
 });
