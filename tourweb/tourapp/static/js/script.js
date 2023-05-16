5ea9c909c7e9f7a7e629e667edaddcbbae480b36
csj0606 <csjoon0606@gmail.com>
2023-05-16 22:12:56 +0900

    5.16

--- tourweb/tourapp/static/js/script.js
+++ tourweb/tourapp/static/js/script.js
@@ -1,16 +1,26 @@
 // 모든 부모 div 요소를 선택합니다.
 const parentDivs = document.querySelectorAll(".span1");
+const bcatTag = document.querySelectorAll(".bact1");
 
 parentDivs.forEach(parentDiv => {
   const hiddenDiv = parentDiv.querySelector(".bcat1");
 
   parentDiv.addEventListener("mouseover", function() {
     hiddenDiv.style.display = "block";
-    parantDiv.style.opacity = "0,5";
   });
 
   parentDiv.addEventListener("mouseout", function() {
     hiddenDiv.style.display = "none";
-    parantDiv.style.opacity = "1"; 
+  });
+});
+
+bcatTag.forEach(bcat => {
+  const mcat = bact.querySelector(".bcat");
+
+  bact.addEventListener("click", function() {
+    mcat.style.display = "block";
+  });
+  bcat.addEventListener("mouseout", function() {
+    mcat.style.display = "none";
   });
 });
