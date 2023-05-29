2098b3a0e36ede86df0730fd10cdd2fc4e8fce16
csj0606 <csjoon0606@gmail.com>
2023-05-29 22:23:25 +0900

    5.29

--- tourweb/tourapp/static/js/script.js
+++ tourweb/tourapp/static/js/script.js
@@ -1,31 +1,30 @@
 const parentDivs = document.querySelectorAll(".span1");
 
-parentDivs.forEach(parentDiv => {
+parentDivs.forEach((parentDiv) => {
   const hiddenDiv = parentDiv.querySelector(".bcat1");
   const mcats = hiddenDiv.querySelectorAll(".mcat");
 
-  parentDiv.addEventListener("mouseover", function() {
+  parentDiv.addEventListener("mouseover", function () {
     hiddenDiv.style.display = "block";
   });
 
-  parentDiv.addEventListener("mouseleave", function() {
+  parentDiv.addEventListener("mouseleave", function () {
     hiddenDiv.style.color = "black";
     hiddenDiv.style.display = "none";
-    mcats.forEach((mcat)=> {
+    mcats.forEach((mcat) => {
       mcat.style.display = "none";
     });
   });
 
-  hiddenDiv.addEventListener("click", function(){
+  hiddenDiv.addEventListener("click", function () {
     hiddenDiv.style.color = "white";
-    mcats.forEach((mcat)=> {
+    mcats.forEach((mcat) => {
       mcat.style.display = "block";
     });
   });
-
 });
 
-window.addEventListener("load", function (){
+window.addEventListener("load", function () {
   var logo = this.document.getElementById("logo");
   logo.classList.add("logo_visible");
 });
