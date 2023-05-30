18c0e161c3122da11f7965036cbe5b509fa50bb4
HYUHO LEE <leehh41563636@gmail.com>
2023-05-30 18:57:04 +0900

    result_view 함수 추가

--- tourweb/tourapp/views.py
+++ tourweb/tourapp/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+import urlmake
+import json
+import requests
+
+def result_view(request):
+    keyword = "에버"
+    link = urlmake.KeywordLmaker(keyword)
+    url = requests.get(link.Create())
+    text = url.text
+    data = json.loads(text)
+    
+    context = {
+        'data': data
+    }
+    
+    return render(request, 'result.html', context)
+
+
+
 
 def index(request):
     return render(request, 'tourapp/index.html')
