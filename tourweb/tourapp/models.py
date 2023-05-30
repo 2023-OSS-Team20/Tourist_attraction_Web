7d65cb2e3c96173ecea194344770bbb758436e43
HYUHO LEE <leehh41563636@gmail.com>
2023-05-30 18:27:18 +0900

    json request 후 dict로 정리

--- tourweb/tourapp/models.py
+++ tourweb/tourapp/models.py
@@ -1,2 +1,16 @@
+import urlmake
+import json
+import requests
 from django.db import models
 
+keyword = "에버"
+
+klink = urlmake.KeywordLmaker(keyword)
+
+url = requests.get(klink.Create())
+text = url.text
+
+data = json.loads(text)
+
+
+print(data)
