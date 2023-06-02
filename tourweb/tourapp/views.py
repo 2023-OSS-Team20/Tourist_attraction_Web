8ede7feae3742c56a4400b74bd75df875d50471f
HYUHO LEE <leehh41563636@gmail.com>
2023-06-02 13:51:02 +0900

    class_result 함수 추가

--- tourweb/tourapp/views.py
+++ tourweb/tourapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from urlmake import KeywordLmaker
+from urlmake import KeywordLmaker, ClassLmaker, IdLMaker
 import json
 import requests
 
 
-def result(request):
+
+def Keyword_result(request):
     keyword = "에버"
     link = KeywordLmaker(keyword)
     url = requests.get(link.Create())
@@ -14,10 +15,22 @@ def result(request):
     context = {
         'data': data
     }
-    
     return render(request, 'tourapp/result.html', context)
 
-def 
+def class_result(request):
+    big = "A01"
+    mid = "A0101"
+    small = None
+    
+    link = ClassLmaker(big, mid, small)
+    url = requests.get(link.Create())
+    text = url.text
+    data = json.loads(text)
+    
+    context = {
+        'data': data
+    }
+    return render(request, 'tourapp/result.html', context)
 
 
 def index(request):
