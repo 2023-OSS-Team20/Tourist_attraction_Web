9af13e30a163eb4efebd1b689cbc1361531920d4
csj0606 <csjoon0606@gmail.com>
2023-06-02 20:38:48 +0900

    urlmake import 오류 수정

--- tourweb/tourapp/views.py
+++ tourweb/tourapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from urlmake import Linkmaker,KeywordLmaker, ClassLmaker, IdLMaker
+from tourapp.urlmake import Linkmaker, KeywordLmaker, ClassLmaker, IdLMaker
 import json
 import requests
 
