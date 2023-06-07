f77c1000f50b5bf9320c5e5eceac76e3dee2405f
cmc4647 <127467135+cmc4647@users.noreply.github.com>
2023-06-07 23:05:42 +0900

    줄이기

--- using_api_practice.py.py
+++ using_api_practice.py.py
@@ -1,49 +1,262 @@
-
-   #하위페이지 시작
-   <!DOCTYPE html>
-<html>
-<head>
-  <title>하위 페이지 1</title>
-</head>
-<body>
-  <h1>하위 페이지 1</h1>
-  <p>.</p>
-</body>
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <title id="usrTitle">TourAPI4.0</title>
+        <meta name="description" content="누구나 쉽게 접근하고 활용 할 수 있는 다국어 관광정보 고객 맞춤형 데이터">
+        <meta name="keywords" content="TourAPI, 다국어 9종 관광정보 실시간 제공, 관광정보 OpenAPI">
+        <meta property="og:title" content="TourAPI4.0"/>
+        <meta property="og:description" content="누구나 쉽게 접근하고 활용 할 수 있는 다국어 관광정보 고객 맞춤형 데이터"/>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        <script>
+            "http" == window.location.href.split("://")[0] && (window.location = "https://" + window.location.href.split("://")[1])
+        </script>
+        <script type="text/javascript" src="http://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=vi10l9w3ql"></script>
+        <script type="text/javascript" src="http://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=vi10l9w3ql&callback=CALLBACK_CUNCTION"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MW0W7QWP25"></script>
+        <script>
+            function gtag() {
+                dataLayer.push(arguments)
+            }
+            window.dataLayer = window.dataLayer || [],
+            gtag("js", new Date),
+            gtag("config", "G-MW0W7QWP25")
+        </script>
+        <link href="/static/css/0.86dffb2d.chunk.css" rel="stylesheet">
+        <link href="/static/css/1.d450fd16.chunk.css" rel="stylesheet">
+    </head>
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="tourApi"></div>
+        <div id="modal-root"></div>
+        <script>
+            !function(e) {
+                function t(t) {
+                    for (var n, o, c = t[0], i = t[1], f = t[2], l = 0, d = []; l < c.length; l++)
+                        o = c[l],
+                        Object.prototype.hasOwnProperty.call(a, o) && a[o] && d.push(a[o][0]),
+                        a[o] = 0;
+                    for (n in i)
+                        Object.prototype.hasOwnProperty.call(i, n) && (e[n] = i[n]);
+                    for (s && s(t); d.length; )
+                        d.shift()();
+                    return u.push.apply(u, f || []),
+                    r()
+                }
+                function r() {
+                    for (var e, t = 0; t < u.length; t++) {
+                        for (var r = u[t], n = !0, o = 1; o < r.length; o++) {
+                            var i = r[o];
+                            0 !== a[i] && (n = !1)
+                        }
+                        n && (u.splice(t--, 1),
+                        e = c(c.s = r[0]))
+                    }
+                    return e
+                }
+                var n = {}
+                  , o = {
+                    15: 0
+                }
+                  , a = {
+                    15: 0
+                }
+                  , u = [];
+                function c(t) {
+                    if (n[t])
+                        return n[t].exports;
+                    var r = n[t] = {
+                        i: t,
+                        l: !1,
+                        exports: {}
+                    };
+                    return e[t].call(r.exports, r, r.exports, c),
+                    r.l = !0,
+                    r.exports
+                }
+                c.e = function(e) {
+                    var t = [];
+                    o[e] ? t.push(o[e]) : 0 !== o[e] && {
+                        2: 1,
+                        16: 1
+                    }[e] && t.push(o[e] = new Promise((function(t, r) {
+                        for (var n = "static/css/" + ({}[e] || e) + "." + {
+                            2: "f2d8f97e",
+                            3: "31d6cfe0",
+                            4: "31d6cfe0",
+                            5: "31d6cfe0",
+                            6: "31d6cfe0",
+                            7: "31d6cfe0",
+                            8: "31d6cfe0",
+                            9: "31d6cfe0",
+                            16: "58e273b9"
+                        }[e] + ".chunk.css", a = c.p + n, u = document.getElementsByTagName("link"), i = 0; i < u.length; i++) {
+                            var f = (s = u[i]).getAttribute("data-href") || s.getAttribute("href");
+                            if ("stylesheet" === s.rel && (f === n || f === a))
+                                return t()
+                        }
+                        var l = document.getElementsByTagName("style");
+                        for (i = 0; i < l.length; i++) {
+                            var s;
+                            if ((f = (s = l[i]).getAttribute("data-href")) === n || f === a)
+                                return t()
+                        }
+                        var d = document.createElement("link");
+                        d.rel = "stylesheet",
+                        d.type = "text/css",
+                        d.onload = t,
+                        d.onerror = function(t) {
+                            var n = t && t.target && t.target.src || a
+                              , u = new Error("Loading CSS chunk " + e + " failed.\n(" + n + ")");
+                            u.code = "CSS_CHUNK_LOAD_FAILED",
+                            u.request = n,
+                            delete o[e],
+                            d.parentNode.removeChild(d),
+                            r(u)
+                        }
+                        ,
+                        d.href = a,
+                        document.getElementsByTagName("head")[0].appendChild(d)
+                    }
+                    )).then((function() {
+                        o[e] = 0
+                    }
+                    )));
+                    var r = a[e];
+                    if (0 !== r)
+                        if (r)
+                            t.push(r[2]);
+                        else {
+                            var n = new Promise((function(t, n) {
+                                r = a[e] = [t, n]
+                            }
+                            ));
+                            t.push(r[2] = n);
+                            var u, i = document.createElement("script");
+                            i.charset = "utf-8",
+                            i.timeout = 120,
+                            c.nc && i.setAttribute("nonce", c.nc),
+                            i.src = function(e) {
+                                return c.p + "static/js/" + ({}[e] || e) + "." + {
+                                    2: "bad9d9ab",
+                                    3: "0b4d358c",
+                                    4: "9001642c",
+                                    5: "e575aa6a",
+                                    6: "fd756238",
+                                    7: "83b04984",
+                                    8: "baadf11f",
+                                    9: "282676a8",
+                                    16: "6ae9dd4a"
+                                }[e] + ".chunk.js"
+                            }(e);
+                            var f = new Error;
+                            u = function(t) {
+                                i.onerror = i.onload = null,
+                                clearTimeout(l);
+                                var r = a[e];
+                                if (0 !== r) {
+                                    if (r) {
+                                        var n = t && ("load" === t.type ? "missing" : t.type)
+                                          , o = t && t.target && t.target.src;
+                                        f.message = "Loading chunk " + e + " failed.\n(" + n + ": " + o + ")",
+                                        f.name = "ChunkLoadError",
+                                        f.type = n,
+                                        f.request = o,
+                                        r[1](f)
+                                    }
+                                    a[e] = void 0
+                                }
+                            }
+                            ;
+                            var l = setTimeout((function() {
+                                u({
+                                    type: "timeout",
+                                    target: i
+                                })
+                            }
+                            ), 12e4);
+                            i.onerror = i.onload = u,
+                            document.head.appendChild(i)
+                        }
+                    return Promise.all(t)
+                }
+                ,
+                c.m = e,
+                c.c = n,
+                c.d = function(e, t, r) {
+                    c.o(e, t) || Object.defineProperty(e, t, {
+                        enumerable: !0,
+                        get: r
+                    })
+                }
+                ,
+                c.r = function(e) {
+                    "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+                        value: "Module"
+                    }),
+                    Object.defineProperty(e, "__esModule", {
+                        value: !0
+                    })
+                }
+                ,
+                c.t = function(e, t) {
+                    if (1 & t && (e = c(e)),
+                    8 & t)
+                        return e;
+                    if (4 & t && "object" == typeof e && e && e.__esModule)
+                        return e;
+                    var r = Object.create(null);
+                    if (c.r(r),
+                    Object.defineProperty(r, "default", {
+                        enumerable: !0,
+                        value: e
+                    }),
+                    2 & t && "string" != typeof e)
+                        for (var n in e)
+                            c.d(r, n, function(t) {
+                                return e[t]
+                            }
+                            .bind(null, n));
+                    return r
+                }
+                ,
+                c.n = function(e) {
+                    var t = e && e.__esModule ? function() {
+                        return e.default
+                    }
+                    : function() {
+                        return e
+                    }
+                    ;
+                    return c.d(t, "a", t),
+                    t
+                }
+                ,
+                c.o = function(e, t) {
+                    return Object.prototype.hasOwnProperty.call(e, t)
+                }
+                ,
+                c.p = "/",
+                c.oe = function(e) {
+                    throw console.error(e),
+                    e
+                }
+                ;
+                var i = this.webpackJsonpfrontend = this.webpackJsonpfrontend || []
+                  , f = i.push.bind(i);
+                i.push = t,
+                i = i.slice();
+                for (var l = 0; l < i.length; l++)
+                    t(i[l]);
+                var s = f;
+                r()
+            }([])
+        </script>
+        <script src="/static/js/0.497fad1a.chunk.js"></script>
+        <script src="/static/js/1.a00afbfa.chunk.js"></script>
+        <script src="/static/js/public.fc976fb9.9b3a1726.chunk.js"></script>
+    </body>
 </html>
-
-
-...
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-  <style>
-    #info {
-      display: none;
-    }
-  </style>
-</head>
-<body>
-  <button onclick="showInfo()"></button>
-  <div id="info"></div>
-
-  <script>
-    function showInfo() {
-      var infoDiv = document.getElementById("info");
-      if (infoDiv.style.display === "none") {
-        infoDiv.style.display = "block";
-      } else {
-        infoDiv.style.display = "none"; 
-      }
-    }
-  </script>
-</body>
-</html>
-...
-<div>
-    <a href="#1">name</a>
-</div>
-
-<p id="1" style="margin-top:1500px;">name</p>
 
 
